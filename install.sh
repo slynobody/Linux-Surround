8de5fe94c5b97d36d1775c3354aa3ec1fa037c91
slynobody <55579266+slynobody@users.noreply.github.com>
2025-08-09 13:27:13 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 mkdir -p ~/.config/pipewire/pipewire.conf.d
-
 USER_HOME=$(eval echo "~$USER")
 sed "s|/home/deck|$USER_HOME|g" ./sink-virtual-surround-7.1.conf > ~/.config/pipewire/pipewire.conf.d/sink-virtual-surround-7.1.conf
-
 touch  ~/.config/pipewire/pipewire.conf.d/virtual-sink.conf
 cp ./oal_dflt.wav ./atmos.wav ~/.config/pipewire
 #from  https://airtable.com/appayGNkn3nSuXkaz/shruimhjdSakUPg2m/tbloLjoZKWJDnLtTc
-sudo systemctl --user restart wireplumber pipewire pipewire-pulse
+systemctl --user restart wireplumber pipewire pipewire-pulse
